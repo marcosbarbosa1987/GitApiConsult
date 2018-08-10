4b7c14498d0792eeeb296fb94953ff3b01cb308c
Marcos Barbosa <marcosbarbosa1987@live.com>
2018-08-10 12:01:31 -0300

    Add Languages.swift, create viewSearch, add buttonSearch, addTextField

---
+++ GitTest/Model/Languages.swift
@@ -0,0 +1,32 @@
+//
+//  Languages.swift
+//  GitTest
+//
+//  Created by Marcos Barbosa on 10/08/18.
+//  Copyright © 2018 n/a. All rights reserved.
+//
+
+import Foundation
+
+//enum Languages {
+//    case JavaScript
+//    case HTML
+//    case CSS
+//    case SQL
+//    case Java
+//    case Python
+//    case C#
+//    case PHP
+//    case C++
+//    case C
+//    case Typescript
+//    case Ruby
+//    case Swift
+//    case Assembly
+//    case Go
+//    case Objective-C
+//    case VB.NET
+//    case R
+//    case Matlab
+//}
+
--- GitTest/Scenes/Home/HomeController.swift
+++ GitTest/Scenes/Home/HomeController.swift
@@ -26,6 +26,31 @@ class HomeController: UIViewController {
     var isEnd = false
     var repositories = [Repository]()
     var loading: UIViewController!
+    var heightConstraint: NSLayoutConstraint!
+    var booleanHeightSearch = false
+    
+    var viewSearch: UIView = {
+        var view = UIView()
+        view.layer.backgroundColor = UIColor.red.cgColor
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    var textFieldLanguage: UITextField = {
+        var textField = UITextField()
+        textField.backgroundColor = UIColor.white
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    var buttonSearch: UIButton = {
+        var button = UIButton()
+        button.backgroundColor = #colorLiteral(red: 0, green: 0.5603182912, blue: 0, alpha: 1)
+        button.layer.cornerRadius = 8
+        button.setTitle("Buscar", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
     
     var tableHome: UITableView = {
         var tableView = UITableView()
@@ -45,19 +70,37 @@ class HomeController: UIViewController {
         super.viewWillAppear(animated)
         
         UIApplication.shared.statusBarStyle = .lightContent
+        view.addSubview(viewSearch)
+        viewSearch.addSubview(textFieldLanguage)
+        viewSearch.addSubview(buttonSearch)
+        configureView()
         view.addSubview(tableHome)
         
         self.navigationController?.navigationBar.barTintColor = UIColor(red: 37/255, green: 40/255, blue: 47/255, alpha: 1)
         self.navigationController?.navigationBar.tintColor = UIColor.white
         
-        let addButton = UIBarButtonItem(image: #imageLiteral(resourceName: "baseline_search_white_18dp"), style:.plain, target:self, action: #selector(buttonAction(_:)))
+        let addButton = UIBarButtonItem(image: #imageLiteral(resourceName: "baseline_search_white_18dp"), style:.plain, target:self, action: #selector(buttonAction))
         addButton.tintColor = UIColor.white
         self.navigationItem.rightBarButtonItem = addButton
         
     }
     
-    @objc func buttonAction(_ sender: UIBarButtonItem) {
+    @objc func buttonAction() {
+        if booleanHeightSearch {
+            
+            UIView.animate(withDuration: 0.5, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
+                self.heightConstraint.constant = 0
+                self.view.layoutIfNeeded()
+            }, completion: nil)
+        }else{
+            self.view.layoutIfNeeded()
+            UIView.animate(withDuration: 0.5, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
+                self.heightConstraint.constant = 60
+                self.view.layoutIfNeeded()
+            }, completion: nil)
+        }
         
+        booleanHeightSearch = !booleanHeightSearch
     }
     
     override func viewDidLoad() {
@@ -106,17 +149,38 @@ class HomeController: UIViewController {
 
 extension HomeController: UITableViewDelegate, UITableViewDataSource{
     
+    func configureView(){
+        
+        viewSearch.topAnchor.constraint(equalTo: view.topAnchor, constant: 64).isActive = true
+        viewSearch.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        viewSearch.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        
+        heightConstraint = viewSearch.heightAnchor.constraint(equalToConstant: 0)
+        heightConstraint.isActive = true
+        
+        buttonSearch.topAnchor.constraint(equalTo: viewSearch.topAnchor, constant: 10).isActive = true
+        buttonSearch.trailingAnchor.constraint(equalTo: viewSearch.trailingAnchor, constant: -16).isActive = true
+        buttonSearch.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        buttonSearch.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        textFieldLanguage.topAnchor.constraint(equalTo: viewSearch.topAnchor, constant: 10).isActive = true
+        textFieldLanguage.leadingAnchor.constraint(equalTo: viewSearch.leadingAnchor, constant: 16).isActive = true
+        textFieldLanguage.trailingAnchor.constraint(equalTo: buttonSearch.leadingAnchor, constant: -16).isActive = true
+        textFieldLanguage.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+    }
+    
     override func viewWillLayoutSubviews() {
-
+        
         tableHome.delegate = self
         tableHome.dataSource = self
         tableHome.register(HomeCell.self, forCellReuseIdentifier: "cell")
-
-        tableHome.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        
+        tableHome.topAnchor.constraint(equalTo: viewSearch.bottomAnchor, constant: 0).isActive = true
         tableHome.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
         tableHome.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
         tableHome.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
-
+        
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -160,6 +224,12 @@ extension HomeController: UITableViewDelegate, UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         router.showAnotherVC(navigation: self.navigationController!, source: self, sender: repositories[indexPath.row])
+    }
+    
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        if booleanHeightSearch{
+            buttonAction()
+        }
     }
     
 }
